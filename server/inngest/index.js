28ca7dd9833260a25afb21f4f55b891e89ead213
Alice26-US <alicendankam@gmail.com>
2026-03-21 22:06:36 +0100

    model import fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,52 +1,56 @@
 import { Inngest } from "inngest";
+import User from "../models/User.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "movie tickect booking" });
+export const inngest = new Inngest({ id: "movie_ticket_booking" });
 
-// Inngest Funtion to save user data to the database when a user signs up
+// --- Sync user creation ---
 const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk" }, 
-    {event: "clerk/user.created"},
-    async ({ event }) => {
-      const {id, first_name, last_name, email_addresses, Image_url } = event.data
-      const userData = {
-        _id: id,
-        name: first_name + " " + last_name,
-        email: email_addresses[0].email_address,
-        image: Image_url
-      }
-      await db.User.create(userData)
-    } 
-)
+  { 
+    id: "sync-user-from-clerk", 
+    triggers: { event: "clerk.user.created" },
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, Image_url } = event.data;
+    const userData = {
+      _id: id,
+      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,
+      image: Image_url
+    };
+    await User.create(userData);
+  }
+);
 
-
-// Inngest Funtion to delete user data to the database when a user signs up
+// --- Sync user deletion ---
 const syncUserDeletion = inngest.createFunction(
-  { id: "delete-user-from-clerk" }, 
-    {event: "clerk/user.deleted"},
-    async ({ event }) => {
-        const {id} = event.data
-        await db.User.findByIdAndDelete(id)
-    } 
-)
+  {
+    id: "delete-user-from-clerk",
+    triggers: { event: "clerk.user.deleted" }, // ✅ triggers must be here
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  }
+);
 
-
-// Inngest Funtion to update user data to the database when a user signs up
+// --- Sync user update ---
 const syncUserUpdate = inngest.createFunction(
-  { id: "update-user-from-clerk" }, 
-    {event: "clerk/user.updated"},
-    async ({ event }) => {
-        const {id, first_name, last_name, email_addresses, Image_url } = event.data
-      const userData = {
-        _id: id,
-        name: first_name + " " + last_name,
-        email: email_addresses[0].email_address,
-        image: Image_url
+  {
+    id: "update-user-from-clerk",
+    triggers: { event: "clerk.user.updated" }, // ✅ triggers must be here
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, Image_url } = event.data;
+    const userData = {
+      _id: id,
+      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,
+      image: Image_url
+    };
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
 
-        }
-        await User.findByIdAndUpdate(id, userData)
-    } 
-)
-// Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
-
+// Export all functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate];
